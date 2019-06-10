a583541810805713af3261de99a495674a0bda5a
gabriel096 <gabriellucaslima96@gmail.com>
2019-06-10 00:19:06 -0300

    exercio 02

---
+++ ex2/program.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+        double numeroReal;
+        cout << "Digite um numero real: ";
+        cin >> numeroReal;
+        cout <<  endl << "obrigado o numero digitado foi: " << numeroReal << endl;
+    return 0;
+}
---
+++ ex3/program.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n1, n2, n3;
+    cout << "Digite 3 numeros inteiros" << endl;
+    cout << "n1: ";
+    cin >> n1;
+    cout << "n2: ";
+    cin >> n2;
+    cout << "n3: ";
+    cin >> n3;
+
+    cout << "obrigado os numeros digitados " << n1 << "," << n2 << "," << n3 << endl;
+
+    return 0;
+}
