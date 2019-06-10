cc5cdf6741fe4951780057fff8ba7475e2b72446
gabriel096 <gabriellucaslima96@gmail.com>
2019-06-09 23:54:56 -0300

    ex

---
+++ ex1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int number;
+
+    cout << "\nenter a number: ";
+    cin >> number;
+
+    cout << "number: " << number;
+
+    return 0;
+}
+
